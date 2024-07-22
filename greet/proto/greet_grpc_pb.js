80593682b6c0de3bbe9257605b6707564053392d
Abhishek Chaturvedi <abhishekchaturvedi789@gmail.com>
2024-07-23 01:23:11 +0530

    Client Boilerplate

---
+++ greet/proto/greet_grpc_pb.js
@@ -0,0 +1,10 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('@grpc/grpc-js');
+
+
+var GreetServiceService = exports.GreetServiceService = {
+};
+
+exports.GreetServiceClient = grpc.makeGenericClientConstructor(GreetServiceService);
